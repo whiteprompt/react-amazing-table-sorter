f99665b70bd94d4db3e7024c05536928f81904fc
Damian Aruj <aruj.damian@gmail.com>
2017-06-12 15:13:09 -0300

    Updates

---
+++ app.c08f4a0864a82a08e34a.js
@@ -0,0 +1,1 @@
+webpackJsonp([0],{75:function(e,n){},76:function(e,n){},79:function(e,n,t){"use strict";Object.defineProperty(n,"__esModule",{value:!0});var l=t(78),o=t.n(l),c=t(77),r=t.n(c),u=t(75),a=(t.n(u),t(76));t.n(a);r.a.render(o.a.createElement("div",null,"Hello World"),document.getElementById("app"))}},[79]);
